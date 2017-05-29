a279fbeaa3f4e70af3cab2f5dddebab82a305a6d
RSR <srinivasaraghavan_r@infosys.com>
2017-05-29 23:46:59 +0530

    merge latest changes from ev-foundation to oe-cloud

--- common/mixins/failsafe-observer-mixin.js
+++ common/mixins/failsafe-observer-mixin.js
@@ -1,166 +1,201 @@
-/**
- *
- * �2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
- * Bangalore, India. All Rights Reserved.
- *
- */
 var logger = require('oe-logger');
 var log = logger('failsafe-observer-mixin');
 var async = require('async');
 var UUID = require('node-uuid');
 var eventHistroyManager = require('./../../lib/event-history-manager.js');
 
+var observerTypes = ['after save', 'after delete'];
+
 module.exports = function failsafeObserverMixin(Model) {
-  if (Model.modelName === 'BaseEntity') {
-    return;
-  }
 
-  if (Model.modelName === 'BaseReplayableEntity') {
-    return;
-  }
+    if (Model.modelName === 'BaseEntity') {
+        return;
+    }
 
-  var FailSafeObserver = function failsafeObserverFn(fn) {
-    var _fn = fn;
-    var _id = UUID.v4();
-    var _name = fn.name;
+    if (Model.modelName === 'BaseReplayableEntity') {
+        return;
+    }
 
-    this.execute = function failSafeExecuteFn(modelName, version, ctx, next) {
-      _fn(ctx, function innerExecuteCallbackFn(error) {
-        if (!error) {
-          eventHistroyManager.update(modelName, version, _id);
-          next();
-        } else {
-          log.error(ctx.options, 'an after save observer: ', _fn.name, ' failed with error: ', error.message);
-          next(error);
+    var FailSafeObserver = function(fn) {
+        var _fn = fn;
+        var _id = UUID.v4();
+        var _name = fn.name;
+
+        this.execute = function(modelName, version, operation, ctx, next) {
+            _fn(ctx, function(error) {
+                if (!error) {
+                    if (version) {
+                        eventHistroyManager.update(modelName, version, operation, _id);
+                    }
+                    next();
+                } else {
+                    log.error(ctx.options, 'failSafe observer: ', _fn.name, ' failed with error: ', error.message);
+                    next(error);
+                }
+            });
+        };
+
+        this.getId = function() {
+            return _id;
+        };
+
+        this.getName = function() {
+            return _name;
+        };
+    };
+
+    Model._fsObservers = {};
+
+    Model.defineProperty('_fsCtx', {
+        type: 'string'
+    });
+
+    if (Model.definition.settings.hidden) {
+        Model.definition.settings.hidden = Model.definition.settings.hidden.concat(['_fsCtx']);
+    } else {
+        Model.definition.settings.hidden = ['_fsCtx'];
+    }
+
+    Model.failSafeObserve = function(eventName, observer) {
+        if (!(observer instanceof FailSafeObserver)) {
+            var err = new Error('observer should be an instanceof FailSafeObserver');
+            err.retriable = false;
+            throw err;
         }
-      });
+
+        if (!this._fsObservers[eventName]) {
+            this._fsObservers[eventName] = {'observers': [], 'observerIds': []};
+        }
+
+        this._fsObservers[eventName].observers.push(observer);
+        this._fsObservers[eventName].observerIds.push(observer.getId());
+
     };
 
-    this.getId = function getIdFn() {
-      return _id;
-    };
-
-    this.getName = function getNameFn() {
-      return _name;
-    };
-  };
-
-  Model._fsObservers = {};
-
-  Model.defineProperty('_fsCtx', {
-    type: 'string'
-  });
-
-  if (Model.definition.settings.hidden) {
-    Model.definition.settings.hidden = Model.definition.settings.hidden.concat(['_fsCtx']);
-  } else {
-    Model.definition.settings.hidden = ['_fsCtx'];
-  }
-
-  Model.failSafeObserve = function failSafeOnserveFn(eventName, observer) {
-    if (!(observer instanceof FailSafeObserver)) {
-      var err = new Error('observer should be an instanceof FailSafeObserver');
-      err.retriable = false;
-      throw err;
+    function converteObservers(type) {
+        if (Model._observers[type] !== undefined) {
+            Model._observers[type].forEach(function(observer) {
+                var failSafeObserver = new FailSafeObserver(observer);
+                Model.failSafeObserve(type, failSafeObserver);
+            });
+            Model._observers[type] = [];
+        }
+    }
+    function changeObserve() {
+        var _observe = Model.observe;
+        Model.observe = function(operation, method) {
+            if (observerTypes.find(op => op === operation) && !method.name.startsWith('_failsafeObserver')) {
+                var failSafeObserver = new FailSafeObserver(method);
+                Model.failSafeObserve(operation, failSafeObserver);
+            } else {
+                _observe.call(this, operation, method);
+            }
+        };
     }
 
-    if (!this._fsObservers[eventName]) {
-      this._fsObservers[eventName] = { 'observers': [], 'observerIds': [] };
-    }
+    Model.evObserve('before save', failsafeObserverBeforeSave);
+    Model.evObserve('before delete', failsafeObserverBeforeDelete);
 
-    this._fsObservers[eventName].observers.push(observer);
-    this._fsObservers[eventName].observerIds.push(observer.getId());
-  };
+    observerTypes.forEach(type => converteObservers(type));
+    changeObserve();
 
-  function converteObservers() {
-    if (typeof Model._observers['after save'] !== 'undefined') {
-      Model._observers['after save'].forEach(function modelObserversFn(observer) {
-        var failSafeObserver = new FailSafeObserver(observer);
-        Model.failSafeObserve('after save', failSafeObserver);
-      });
-      Model._observers['after save'] = [];
-    }
-    var _observe = Model.observe;
-    Model.observe = function modelObserveFn(operation, method) {
-      if (operation === 'after save' && method.name !== '_failsafeObserverAfterSave') {
-        var failSafeObserver = new FailSafeObserver(method);
-        Model.failSafeObserve('after save', failSafeObserver);
-      } else {
-        _observe.call(this, operation, method);
-      }
-    };
-  }
+    Model.evObserve('after save', _failsafeObserverAfterSave);
+    Model.evObserve('after delete', _failsafeObserverAfterDelete);
 
-  Model.evObserve('before save', failsafeObserverBeforeSave);
-
-  converteObservers();
-
-  Model.evObserve('after save', _failsafeObserverAfterSave);
 };
 
-function failsafeObserverBeforeSave(ctx, next) {
-  var version;
-  if (typeof ctx.instance !== 'undefined') {
-    version = ctx.instance._version;
-    ctx.instance._fsCtx = JSON.stringify(ctx.options);
-  } else if (typeof ctx.data !== 'undefined') {
-    version = ctx.data._version;
-  }
-  eventHistroyManager.create(ctx.Model.modelName, version, ctx);
-  next();
+function failsafeObserverBeforeDelete(ctx, next) {
+    var version;
+    if (ctx.instance !== undefined) {
+        version = ctx.instance._version;
+        ctx.instance._fsCtx = JSON.stringify(ctx.options);
+    } else if (ctx.data !== undefined) {
+        version = ctx.data._version;
+    }
+    if (!version) {
+        return next();
+    }
+    eventHistroyManager.create(ctx.Model.modelName, version, 'after delete', ctx);
+    next();
 }
 
+function failsafeObserverBeforeSave(ctx, next) {
+    var version;
+    if (ctx.instance !== undefined) {
+        version = ctx.instance._version;
+        ctx.instance._fsCtx = JSON.stringify(ctx.options);
+    } else if (ctx.data !== undefined) {
+        version = ctx.data._version;
+    }
+    if (!version) {
+        return next();
+    }
+
+    eventHistroyManager.create(ctx.Model.modelName, version, 'after save', ctx);
+    next();
+}
 
 function _failsafeObserverAfterSave(ctx, next) {
-  // Invoke failsafe observers in look
-  if (ctx.Model.definition.settings.mixins.FailsafeObserverMixin) {
-    notifyFailsafeObservers(ctx.Model, ctx.Model, ctx, function notifyFailsafeObserversFn(err) {
-      var version = (ctx.instance && ctx.instance._version) || (ctx.data && ctx.data._version);
-      if (!err) {
-        eventHistroyManager.remove(ctx.Model.modelName, version);
-        next();
-      } else {
-        if (err.retriable === false) {
-          eventHistroyManager.remove(ctx.Model.modelName, version, 'error: ' + err.message + ' is not retriable.');
-          return next(err);
-        }
-        next();
-      }
-    });
-  } else {
-    next();
-  }
+    invokeFailsafeObserver(ctx, 'after save', next);
 }
 
-function notifyFailsafeObservers(childModel, model, ctx, cb) {
-  notifyBaseFailsafeObservers(childModel, model, ctx, function notifyBaseFailsafeObserversCbFn(error) {
-    if (error) {
-      return cb(error);
+function _failsafeObserverAfterDelete(ctx, next) {
+    invokeFailsafeObserver(ctx, 'after delete', next);
+}
+
+function invokeFailsafeObserver(ctx, operation, next) {
+    if (ctx.Model.definition.settings.mixins.FailsafeObserverMixin) {
+        notifyFailsafeObservers(ctx.Model, ctx.Model, operation, ctx, function(err) {
+            var version = (ctx.instance && ctx.instance._version) || (ctx.data && ctx.data._version);
+            if (!err) {
+                if (version) {
+                    eventHistroyManager.remove(ctx.Model.modelName, version, operation);
+                }
+                return next();
+            } else {
+                if (err.retriable === false) {
+                    if (version) {
+                        eventHistroyManager.remove(ctx.Model.modelName, version, operation, 'error: ' + err.message + ' is not retriable.');
+                    }
+                    return next(err);
+                }
+                return next();
+            }
+        });
+    } else {
+        return next();
     }
-    var fsObservers = (model._fsObservers['after save'] && model._fsObservers['after save'].observers) || [];
-    var version = (ctx.instance && ctx.instance._version) || (ctx.data && ctx.data._version);
-    async.eachSeries(fsObservers, function notifySingleObserver(fn, callback) {
-      var retval = fn.execute(childModel.modelName, version, ctx, callback);
-
-      if (retval && typeof retval.then === 'function') {
-        retval.then(function retvalFn() {
-          callback();
-        }, callback);
-      }
-    }, function asyncEachSeriesCbFn(err) {
-      if (err) {
-        cb(err);
-      } else {
-        cb();
-      }
-    });
-  });
 }
 
-function notifyBaseFailsafeObservers(childModel, model, ctx, cb) {
-  if (model.base && model.base._fsObservers) {
-    notifyFailsafeObservers(childModel, model.base, ctx, cb);
-  } else {
-    cb();
-  }
+function notifyFailsafeObservers(childModel, model, operation, ctx, cb) {
+    notifyBaseFailsafeObservers(childModel, model, operation, ctx, function(error) {
+        if (error) {
+            return cb(error);
+        }
+        var fsObservers = (model._fsObservers[operation] && model._fsObservers[operation].observers) || [];
+        var version = (ctx.instance && ctx.instance._version) || (ctx.data && ctx.data._version);
+        async.eachSeries(fsObservers, function notifySingleObserver(fn, callback) {
+            var retval = fn.execute(childModel.modelName, version, operation, ctx, callback);
+
+            if (retval && typeof retval.then === 'function') {
+                retval.then(function() {
+                    callback();
+                }, callback);
+            }
+        }, function(err) {
+            if (err) {
+                cb(err);
+            } else {
+                cb();
+            }
+        });
+    });
+}
+
+function notifyBaseFailsafeObservers(childModel, model, operation, ctx, cb) {
+    if (model.base && model.base._fsObservers) {
+        notifyFailsafeObservers(childModel, model.base, operation, ctx, cb);
+    } else {
+        cb();
+    }
 }
--- test/failsafe-observer-test.js
+++ test/failsafe-observer-test.js
@@ -1,9 +1,9 @@
-/**
- * 
- * ©2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
- * Bangalore, India. All Rights Reserved.
- * 
- */
+/*
+©2015-2016 EdgeVerve Systems Limited (a fully owned Infosys subsidiary), Bangalore, India. All Rights Reserved.
+The EdgeVerve proprietary software program ("Program"), is protected by copyrights laws, international treaties and other pending or existing intellectual property rights in India, the United States and other countries.
+The Program may contain/reference third party or open source components, the rights to which continue to remain with the applicable third party licensors or the open source community as the case may be and nothing here transfers the rights to the third party and open source components, except as expressly permitted.
+Any unauthorized reproduction, storage, transmission in any form or by any means (including without limitation to electronic, mechanical, printing, photocopying, recording or  otherwise), or any distribution of this Program, or any portion of it, may result in severe civil and criminal penalties, and will be prosecuted to the maximum extent possible under the law.
+*/
 
 /**
  *
@@ -76,7 +76,7 @@ describe('failsafe-observer-mixin', function () {
     function createModelDefinition(modelSchema, cb) {
         models.ModelDefinition.create(modelSchema, defaultContext, function (err, res) {
             if (err) {
-                debug('error in create test model', err);
+                console.log('error in create test model', err);
                 cb(err);
             } else {
                 modelSchema._version = undefined;
