aa75b06f3253f3c0ccf2261854a6571725cb9e2a
Pradeep Kumar Tippa <pradeep_tippa@edgevere.com>
2017-06-30 22:05:38 +0530

    Request and Response Logging.

---
+++ server/boot/response-logger.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * ©2016-2017 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
+ * Bangalore, India. All Rights Reserved.
+ *
+ */
+// Not able to use routes:after - middleware to log the response due to 
+// https://github.com/strongloop/loopback/issues/1234#issuecomment-158415183
+var log = require('oe-logger')('response-logger');
+
+module.exports = function responseLogger(app) {
+  app.remotes().after('**', function afterRemoteListner(ctx, next) {
+    log.debug(ctx.req.callContext, ctx.res.statusCode, ' Body: ', JSON.stringify(ctx.result), ' Headers: ', ctx.res._headers);
+    next();
+  });
+};
