69804cace59053cdcf38e673ac382320f358f7cf
Karin Angel <karin.angel@edgeverve.com>
2017-07-16 18:12:19 +0300

    removing log in failsafe mixin

--- common/mixins/failsafe-observer-mixin.js
+++ common/mixins/failsafe-observer-mixin.js
@@ -58,7 +58,6 @@ module.exports = function failsafeObserverMixin(Model) {
           }
           next();
         } else {
-          console.log('failSafe observer: ', _fn.name, ' failed with error: ', error.message);
           log.debug(ctx.options, 'failSafe observer: ', _fn.name, ' failed with error: ', error.message);
           next(error);
         }
