832e9011994677c3e8d3f804fdc1b4ee272a3c7a
Karin Angel <karin.angel@edgeverve.com>
2017-07-18 19:53:54 +0300

    added logs and fixed continueRecovery to map for each hostname

---
+++ common/models/framework/note.js
@@ -0,0 +1,25 @@
+module.exports = function (Model) {
+  Model.prototype.remote2 = function (cb) {
+    cb(null, {message: 'remote 2 ok'});
+  };
+
+  Model.remoteMethod('remote2', {
+    isStatic: false,
+    description: 'remote2',
+    accessType: 'READ',
+    accepts: [
+    ],
+    http: {
+      verb: 'GET',
+      path: '/remote2'
+    },
+    returns: {
+      type: 'object',
+      root: true
+    }});
+
+  Model.observe('after save', function (ctx, next) {
+    var err = new Error('Note after save fail');
+    return next(err);
+  });
+};
