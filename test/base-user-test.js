5e4db38a6eb6459cb65e346caf4a2603f4aa923b
Pradeep Kumar Tippa <pradeep_tippa@edgevere.com>
2017-08-04 16:17:16 +0530

    BaseUser Test cases, added for Login, Realm, Switch-Tenant, Logout

--- test/base-user-test.js
+++ test/base-user-test.js
@@ -22,6 +22,9 @@ var loopback = require('loopback');
 var baseUserModel = loopback.findModel('BaseUser');
 var access_token;
 var userEmail = 'basetestuser@mycompany.com';
+var testUserId = 21;
+var testTenant = 'test-tenant';
+var testRole = 'admin';
 var credentials = {
     username: 'basetestuser',
     password:  'basetestuser123'
@@ -32,10 +35,26 @@ describe(chalk.blue('BaseUser Test'), function () {
             username: credentials.username,
             password: credentials.password,
             email: userEmail,
-            tenantId: 'test-tenant',
-            id: 21
+            tenantId: testTenant,
+            id: testUserId
         };
-        bootstrap.createTestUser(testUser, 'admin', function() {
+        bootstrap.createTestUser(testUser, testRole, function() {{ctx: {tenantId: testTenant}}
+            done();
+        });
+    });
+    it('Call Setup', function(done){
+        baseUserModel.setup();
+        done();
+    })
+    it('Login with valid credentials', function(done) {
+        var postUrl = baseUrl + '/BaseUsers/login';
+        api.set('Accept', 'application/json')
+        .set('tenant_id', testTenant)
+        .post(postUrl)
+        .send(credentials)
+        .end(function(err, response) {
+            expect(response.body.id).to.be.defined;
+            access_token = response.body.id;
             done();
         });
     });
@@ -46,7 +65,7 @@ describe(chalk.blue('BaseUser Test'), function () {
         };
         var postUrl = baseUrl + '/BaseUsers/login';
         api.set('Accept', 'application/json')
-            .set('tenant_id', 'test-tenant')
+            .set('tenant_id', testTenant)
             .post(postUrl)
             .send(postData)
             .expect(401).end(function(err, response) {
@@ -54,10 +73,10 @@ describe(chalk.blue('BaseUser Test'), function () {
                 done();
             });
     });
-    it('Login with valid credentials', function(done) {
+    it('Login with valid credentials again to update failedTries to 0', function(done) {
         var postUrl = baseUrl + '/BaseUsers/login';
         api.set('Accept', 'application/json')
-        .set('tenant_id', 'test-tenant')
+        .set('tenant_id', testTenant)
         .post(postUrl)
         .send(credentials)
         .end(function(err, response) {
@@ -69,7 +88,7 @@ describe(chalk.blue('BaseUser Test'), function () {
     it('Get Session without access_token', function(done) {
         var url = baseUrl + '/BaseUsers/session';
         api.set('Accept', 'application/json')
-            .set('tenant_id', 'test-tenant')
+            .set('tenant_id', testTenant)
             .get(url)
             .expect(200).end(function(err, response) {
                 if (err) {
@@ -83,18 +102,39 @@ describe(chalk.blue('BaseUser Test'), function () {
     it('Get Session with access_token', function(done) {
         var url = baseUrl + '/BaseUsers/session?access_token=' + access_token;
         api.set('Accept', 'application/json')
-            .set('tenant_id', 'test-tenant')
+            .set('tenant_id', testTenant)
             .get(url)
             .expect(200).end(function(err, response) {
                 if (err) {
                     done(err);
                 } else {
+                    expect(response).not.to.be.null;
+                    expect(response).not.to.be.undefined;
+                    expect(response.body).not.to.be.null;
+                    expect(response.body).not.to.be.undefined;
+                    expect(response.body.username).to.be.equal(credentials.username);
+                    expect(response.body.userid).to.be.equal(testUserId);
+                    expect(response.body.tenantId).to.be.equal(testTenant);
+                    expect(response.body.roles).to.be.an('array').that.includes(testRole);
                     done();
                 }
             });
     });
+    /*
+    it('From NODE API without passing options', function(done) {
+        var ctx = {
+                req: {
+                    accessToken: {
+                        id: access_token
+                    }
+                }
+            };
+        baseUserModel.session(ctx, function(err, data){
+            done();
+        })
+    });*/
     it('Reset Password without email', function(done) {
-        baseUserModel.resetPassword({}, {ctx: {tenantId: 'test-tenant'}}, function(err) {
+        baseUserModel.resetPassword({}, {ctx: {tenantId: testTenant}}, function(err) {
             expect(err).not.to.be.null;
             expect(err).not.to.be.undefined;
             expect(err.code).to.be.equal('EMAIL_REQUIRED');
@@ -102,7 +142,7 @@ describe(chalk.blue('BaseUser Test'), function () {
         });
     });
     it('Reset Password invalid email', function(done) {
-        baseUserModel.resetPassword({email: 'tempemailid@test.com'}, {ctx: {tenantId: 'test-tenant'}}, function(err) {
+        baseUserModel.resetPassword({email: 'tempemailid@test.com'}, {ctx: {tenantId: testTenant}}, function(err) {
             // Not returning cb(err) in the callback.
             //expect(err).not.to.be.undefined;
             //expect(err.code).to.be.equal('EMAIL_NOT_FOUND');
@@ -110,9 +150,109 @@ describe(chalk.blue('BaseUser Test'), function () {
         });
     });
     it('Reset Password valid email', function(done) {
-        baseUserModel.resetPassword({email: userEmail}, {ctx: {tenantId: 'test-tenant'}}, function(err) {
+        baseUserModel.resetPassword({email: userEmail}, {ctx: {tenantId: testTenant}}, function(err) {
             expect(err).to.be.undefined;
             done();
+        });
+    });
+    describe(chalk.green('Login'), function() {
+        /*
+        it('Node API With out include, options', function(done) {
+            baseUserModel.login(credentials, function(err, token){
+                console.log("Error ", err);
+                console.log("token ", token);
+                done();
+            });
+        });*/
+        it('Node API With out include', function(done) {
+            baseUserModel.login(credentials, {ctx: {tenantId: testTenant}}, function(err, token){
+                expect(err).to.be.null;
+                expect(token).not.to.be.null;
+                expect(token).not.to.be.undefined;
+                expect(token.tenantId).to.be.equal(testTenant);
+                expect(token.username).to.be.equal(credentials.username);
+                expect(token.userId).to.be.equal(testUserId);
+                expect(token.roles).to.be.an('array').that.includes(testRole);
+                done();
+            });
+        });
+        it('Login without credentials', function(done) {
+            var postUrl = baseUrl + '/BaseUsers/login';
+            api.set('Accept', 'application/json')
+                .set('tenant_id', testTenant)
+                .post(postUrl)
+                .send({})
+                .expect(400).end(function(err, response) {
+                    expect(err).to.be.null;
+                    expect(response).not.to.be.null;
+                    expect(response).not.to.be.undefined;
+                    expect(response.body).not.to.be.null;
+                    expect(response.body).not.to.be.undefined;
+                    expect(response.body.error).not.to.be.null;
+                    expect(response.body.error).not.to.be.undefined;
+                    expect(response.body.error.code).to.be.equal('USERNAME_EMAIL_REQUIRED');
+                    done();
+                });
+        });
+        describe(chalk.yellow('Realm'), function() {
+            var orgRealmReq;
+            before('Setting Realm Required', function(done) {
+                orgRealmReq = baseUserModel.settings.realmRequired;
+                baseUserModel.settings.realmRequired = true;
+                done();
+            });
+            after('Reset to default realm', function(done) {
+                baseUserModel.settings.realmRequired = orgRealmReq;
+                done();
+            });
+            it('Try Login with Realm required', function(done) {
+                var postUrl = baseUrl + '/BaseUsers/login';
+                api.set('Accept', 'application/json')
+                .set('tenant_id', testTenant)
+                .post(postUrl)
+                .send(credentials)
+                .expect(400).end(function(err, response) {
+                    expect(err).to.be.null;
+                    expect(response).not.to.be.null;
+                    expect(response).not.to.be.undefined;
+                    expect(response.body).not.to.be.null;
+                    expect(response.body).not.to.be.undefined;
+                    expect(response.body.error).not.to.be.null;
+                    expect(response.body.error).not.to.be.undefined;
+                    expect(response.body.error.code).to.be.equal('REALM_REQUIRED');
+                    done();
+                });
+            });
+        });
+        describe(chalk.yellow('Email verification required'), function() {
+            var orgEmailVerficationReq;
+            before('Setting Email verification required', function(done) {
+                orgEmailVerficationReq = baseUserModel.settings.emailVerificationRequired;
+                baseUserModel.settings.emailVerificationRequired = true;
+                done();
+            });
+            after('Reset to default Email verification required', function(done) {
+                baseUserModel.settings.emailVerificationRequired = orgEmailVerficationReq;
+                done();
+            });
+            it('Try Login with Email verification required', function(done) {
+                var postUrl = baseUrl + '/BaseUsers/login';
+                api.set('Accept', 'application/json')
+                .set('tenant_id', testTenant)
+                .post(postUrl)
+                .send(credentials)
+                .expect(401).end(function(err, response) {
+                    expect(err).to.be.null;
+                    expect(response).not.to.be.null;
+                    expect(response).not.to.be.undefined;
+                    expect(response.body).not.to.be.null;
+                    expect(response.body).not.to.be.undefined;
+                    expect(response.body.error).not.to.be.null;
+                    expect(response.body.error).not.to.be.undefined;
+                    expect(response.body.error.code).to.be.equal('LOGIN_FAILED_EMAIL_NOT_VERIFIED');
+                    done();
+                });
+            });
         });
     });
     describe(chalk.green('Enabled Regex'), function(){
@@ -129,7 +269,7 @@ describe(chalk.blue('BaseUser Test'), function () {
             // Tried with POST /BaseUsers/login, but it is not triggering validatePassword method.
             // So triggering it manually to increase the code coverage.
             try {
-                baseUserModel.validatePassword(credentials.password, {ctx: {tenantId: 'test-tenant'}});
+                baseUserModel.validatePassword(credentials.password, {ctx: {tenantId: testTenant}});
             } catch (error) {
                 expect(error).not.to.be.undefined;
             }
@@ -140,11 +280,13 @@ describe(chalk.blue('BaseUser Test'), function () {
             done();
         });
     });
-    describe('Disable Account', function() {
+    describe(chalk.green('Disable Account and Unlock'), function() {
         var invalidLoginFns = [];
-        before('populate invalidLoginFns', function(done) {
+        var orgUserAccountTime;
+        before('populate invalidLoginFns and set UNLOCK_USER_ACCOUNT_TIME', function(done) {
             var maxFailedLoginTries = app.get('maxFailedLoginTries') || 5;
-            // Adding 1 more to try accessing the disabled account.
+            orgUserAccountTime = baseUserModel.app.get('UNLOCK_USER_ACCOUNT_TIME');
+            baseUserModel.app.set('UNLOCK_USER_ACCOUNT_TIME', 1000);
             maxFailedLoginTries += 1;
             for(var i=0; i < maxFailedLoginTries; i++) {
                 var fn = function(cb){
@@ -154,7 +296,7 @@ describe(chalk.blue('BaseUser Test'), function () {
                         password: 'invalidpassword'
                     }
                     api.set('Accept', 'application/json')
-                    .set('tenant_id', 'test-tenant')
+                    .set('tenant_id', testTenant)
                     .post(postUrl)
                     .send(invalidCredentials)
                     .end(function(err, response) {
@@ -164,7 +306,11 @@ describe(chalk.blue('BaseUser Test'), function () {
                 invalidLoginFns.push(fn);
             }
             done();
-        })
+        });
+        after('Reset UNLOCK_USER_ACCOUNT_TIME', function(done) {
+            baseUserModel.app.set('UNLOCK_USER_ACCOUNT_TIME', orgUserAccountTime);
+            done();
+        });
         it('Disable account with maxFailedLoginTries', function(done){
             async.series(invalidLoginFns, function(err, responses){
                 // Get last response
@@ -174,8 +320,136 @@ describe(chalk.blue('BaseUser Test'), function () {
                 done();
             });
         });
-        it('Account Unlock', function(done){
-            done();
+        it('Account Unlock valid email', function(done){
+            var postUrl = baseUrl + '/BaseUsers/unlock?access_token=' + access_token;
+            var postdata = {
+                username: credentials.username
+            };
+            api.set('Accept', 'application/json')
+            .set('tenant_id', testTenant)
+            .post(postUrl)
+            .send(postdata)
+            .expect(200).end(function(err, response) {
+                expect(err).to.be.null;
+                expect(response).not.to.be.null;
+                expect(response).not.to.be.undefined;
+                expect(response.status).not.to.be.null;
+                expect(response.status).not.to.be.undefined;
+                done();
+            });
         });
+        it('Account Unlock invalid email', function(done){
+            var postUrl = baseUrl + '/BaseUsers/unlock?access_token=' + access_token;
+            var postdata = {
+                username: 'invalidemail@temp.com'
+            };
+            api.set('Accept', 'application/json')
+            .set('tenant_id', testTenant)
+            .post(postUrl)
+            .send(postdata)
+            .expect(500).end(function(err, response) {
+                expect(response).not.to.be.null;
+                expect(response).not.to.be.undefined;
+                expect(response.body).not.to.be.null;
+                expect(response.body).not.to.be.undefined;
+                expect(response.body.error).not.to.be.null;
+                expect(response.body.error).not.to.be.undefined;
+                done();
+            });
+        });
+    });
+    describe(chalk.green('Switch Tenant'), function() {
+        var accessToken;
+        var switchTenantId = 'default';
+        before('Get New AccessToken', function(done){
+            var postUrl = baseUrl + '/BaseUsers/login';
+            api.set('Accept', 'application/json')
+            .set('tenant_id', testTenant)
+            .post(postUrl)
+            .send(credentials)
+            .end(function(err, response) {
+                accessToken = response.body.id;
+                done();
+            });
+        });
+        it('With AccessToken', function(done){
+            var postUrl = baseUrl + '/BaseUsers/switch-tenant?access_token='+accessToken;
+            var postData = {
+                tenantId: switchTenantId
+            };
+            api.set('Accept', 'application/json')
+            .set('tenant_id', testTenant)
+            .post(postUrl)
+            .send(postData)
+            .expect(200).end(function(err, response) {
+                expect(err).to.be.null;
+                expect(response).not.to.be.null;
+                expect(response).not.to.be.undefined;
+                expect(response.body).not.to.be.null;
+                expect(response.body).not.to.be.undefined;
+                expect(response.body.tenantId).to.be.equal(switchTenantId);
+                done();
+            });
+        });
+        /*
+        // Written the below test to cover code for options not passed
+        // But test cases crash given error in dao that options are not passed.
+        it('From NODE API without options', function(done){
+            var ctx = {
+                req: {
+                    accessToken: {
+                        id: accessToken
+                    }
+                }
+            };
+            baseUserModel.switchTenant(ctx, switchTenantId, function(err, res) {
+                console.log("Response ", res);
+                console.log("err ", err);
+                done();
+            });
+        });*/
+    });
+    describe(chalk.green('Logout'), function() {
+        it('Invalid AccessToken', function(done) {
+            var postUrl = baseUrl + '/BaseUsers/logout?access_token=invalidAccessToken';
+            api.set('Accept', 'application/json')
+            .set('tenant_id', testTenant)
+            .post(postUrl)
+            .send()
+            .expect(500).end(function(err, response) {
+                expect(response).not.to.be.null;
+                expect(response).not.to.be.undefined;
+                expect(response.body).not.to.be.null;
+                expect(response.body).not.to.be.undefined;
+                expect(response.body.error).not.to.be.null;
+                expect(response.body.error).not.to.be.undefined;
+                done();
+            });
+        });
+        it('Valid AccessToken', function(done) {
+            var postUrl = baseUrl + '/BaseUsers/logout?access_token='+access_token;
+            api.set('Accept', 'application/json')
+            .set('tenant_id', testTenant)
+            .post(postUrl)
+            .send()
+            .expect(204).end(function(err, response) {
+                expect(response).not.to.be.null;
+                expect(response).not.to.be.undefined;
+                expect(response.body).to.be.equal('');
+                expect(response.status).to.be.equal(204);
+                done();
+            });
+        });
+        /*
+        // Written the below test to cover code for options not passed
+        // But test cases crash given error in dao that options are not passed.
+        it('From Node API', function(done) {
+            baseUserModel.logout('invalidAccessToken', function(err, resposne) {
+                console.log('Error ', err);
+                console.log('resposne ', resposne);
+                done();
+            });
+        });
+        */
     });
 });
