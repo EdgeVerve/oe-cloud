327f7d2577ff96a9591fedb8f9f59dbf324e62af
David Zharnest <david.zharnest@edgeverve.com>
2017-08-06 10:19:45 +0300

    callback with id

--- common/models/framework/auth-session.js
+++ common/models/framework/auth-session.js
@@ -11,13 +11,14 @@ module.exports = function AuthSessionFn(AuthSession) {
       cb = options;
       options = {};
     }
+    var id = tokenIdForRequest(req, options);
+
     var proxyKey = options.model.app.get('evproxyInternalKey') || '97b62fa8-2a77-458b-87dd-ef64ff67f847';
     if (req.headers && proxyKey) {
       if (req.headers['x-evproxy-internal-key'] === proxyKey) {
-        return cb();
+        return cb(null, id);
       }
     }
-    var id = tokenIdForRequest(req, options);
 
     if (id) {
       this.findById(id, req.callContext, function authSessionFindById(err, token) {
