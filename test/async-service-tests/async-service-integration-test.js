2fdc49bad41f405aa53f9b6f782070bc0df47355
Lior Schindler <lior.schindler@edgeverve.com>
2017-08-17 12:03:04 +0300

    fixing lint in async retry test file

--- test/async-service-tests/async-service-integration-test.js
+++ test/async-service-tests/async-service-integration-test.js
@@ -1,82 +1,80 @@
 var request = require('request');
-var async = require('async');
-var uuid = require('node-uuid');
+
+// var uuid = require('node-uuid');
 var chai = require('chai');
 var expect = chai.expect;
 var chalk = require('chalk');
 
-const serviceName = process.env['APP_IMAGE_NAME'];
-const retryServiceName = process.env['RETRY_SERVICE_NAME'];
-const domainName = process.env['DOMAIN_NAME'];
+const serviceName = process.env.APP_IMAGE_NAME;
+const retryServiceName = process.env.RETRY_SERVICE_NAME;
+const domainName = process.env.DOMAIN_NAME;
 
 const serviceHost = serviceName + '.' + domainName;
 const baseUrl = 'https://' + serviceHost + '/api/';
 const baseRetryUrl = 'https://' + retryServiceName + '.' + domainName + '/api/';
 
-const loginData = {"username":"admin","password":"admin"};
-const testModelName = 'RetryTestModel'
+const loginData = {'username': 'admin', 'password': 'admin'};
+const testModelName = 'RetryTestModel';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 var token;
 
 
-describe(chalk.blue('retry integration tests'), function() {
-
-    before('login to oecloud', function(done) {
-        this.timeout(180000);
-        console.log('Base Url is ', baseurl);
-        request.post(baseurl + "BaseUsers/login", { json: loginData }, function(error, response, body) {
-                if (error || body.error) {
-			        console.log("error:", error || body.error);
-                    done(error || body.error);
-                } else {
-                  token = body.id;
-                  done();
-		        }
-	    });
+describe(chalk.blue('retry integration tests'), function () {
+  before('login to oecloud', function (done) {
+    this.timeout(180000);
+    console.log('Base Url is ', baseUrl);
+    request.post(baseUrl + 'BaseUsers/login', { json: loginData }, function (error, response, body) {
+      if (error || body.error) {
+        console.log('error:', error || body.error);
+        done(error || body.error);
+      } else {
+        token = body.id;
+        done();
+      }
     });
+  });
 
-    it('should post to async retry service wait 20 seconds and check that the post was successful', function(done) {
-        var postData = {
-            blabla: 'abcdefg',
-            amount: 10,
-            status: 'testing' 
-        };
-        var url = baseRetryUrl + testModelName + '?access_token=' + token;
-        var headers = {
-            'x-host': serviceHost
-        }
-        var options = {
-            url: url,
-            headers: headers,
-            json: postData
-        };
-        console.log('posting with ', options);
-        request.post(options, function(error, response, body) {
+  it('should post to async retry service wait 20 seconds and check that the post was successful', function (done) {
+    var postData = {
+      blabla: 'abcdefg',
+      amount: 10,
+      status: 'testing'
+    };
+    var url = baseRetryUrl + testModelName + '?access_token=' + token;
+    var headers = {
+      'x-host': serviceHost
+    };
+    var options = {
+      url: url,
+      headers: headers,
+      json: postData
+    };
+    console.log('posting with ', options);
+    request.post(options, function (error, response, body) {
+      if (error || body.error) {
+        console.log('error:', error || body.error);
+        done(error || body.error);
+      } else {
+        console.log('post response body:', body);
+        expect(response.statusCode).to.equal(200);
+        expect(body.id).to.equal(postData.blabla);
+        setTimeout(checkPost, 20000);
+        function checkPost() {
+          request.get(baseUrl + testModelName + '/' + postData.blabla + '?access_token=' + token, function (error, response, body) {
             if (error || body.error) {
-                console.log("error:", error || body.error);
-                done(error || body.error);
+              console.log('error:', error || body.error);
+              done(error || body.error);
             } else {
-                console.log('post response body:', body);
-                expect(response.statusCode).to.equal(200);
-                expect(body.id).to.equal(postData.blabla);
-                setTimeout(checkPost, 20000);
-                function checkPost() {
-                    request.get(baseurl + testModelName + '/' + postData.blabla + '?access_token=' + token, function(error, response, body) {
-                        if (error || body.error) {
-                            console.log("error:", error || body.error);
-                            done(error || body.error);
-                        } else {
-                            console.log('get response body:', body);
-                            expect(response.statusCode).to.equal(200);
-                            expect(body).to.include(postData);
-                            done();
-                        }
-                    });
-                }
+              console.log('get response body:', body);
+              expect(response.statusCode).to.equal(200);
+              expect(body).to.include(postData);
+              done();
             }
-	    });
+          });
+        }
+      }
     });
-
+  });
 });
