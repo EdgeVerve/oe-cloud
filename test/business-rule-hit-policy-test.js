972c0f89d8a74f99d9569ad55bf481c7ab393538
Ramesh <rameshchoudhary_s@@edgeverve.com>
2017-08-21 15:04:33 +0530

    changes in readfile

--- test/business-rule-hit-policy-test.js
+++ test/business-rule-hit-policy-test.js
@@ -18,32 +18,33 @@ var decisionTableRules = ['RoutingRules', 'ElectricityBill', 'Holidays', 'Member
 var decision_table = {};
 
 describe(chalk.blue('Decision table evaluation'), function () {
-    this.timeout(20000);
+    this.timeout(60000);
     before('Create DecisionTables', function (done) {
-        var datatext = fs.readFile(path.join(__dirname, 'business-rule-data', 'DecisionTable.json'), function (err, data){
-            DTData = JSON.parse(data);
-            async.eachSeries(DTData, function (decisionTable, callback) {
-                models.DecisionTable.create(decisionTable, bootstrap.defaultContext, function (err, data) {
-                    if (err) {
-					    console.log(err);
-                        callback(err);
-                    } else {
-                        callback();
-                    }
-                });
-            }, function (err) {
+        const filepath = path.resolve(path.join(__dirname, 'business-rule-data', 'DecisionTable.json'));
+        const DTData = require(filepath);
+
+        async.eachSeries(DTData, function (decisionTable, callback) {
+            models.DecisionTable.create(decisionTable, bootstrap.defaultContext, function (err, data) {
                 if (err) {
-				    console.log(err);
-                    done(err);
+                    console.log(err);
+                    callback(err);
                 } else {
-                    done();
+                    callback();
                 }
             });
-	    });
+        }, function (err) {
+            if (err) {
+                console.log(err);
+                done(err);
+            } else {
+                done();
+            }
+        });
+
     });
-    
+
     it('Priority hit policy', function (done) {
-        var payload = { "Applicant Age" : 70, "Medical History" : "bad" };
+        var payload = { "Applicant Age": 70, "Medical History": "bad" };
         models.DecisionTable.exec("ApplicantRiskRating", payload, bootstrap.defaultContext, function (err, result) {
             expect(result['Applicant Risk Rating']).to.equal("High");
             done();
@@ -51,7 +52,7 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Output hit policy', function (done) {
-        var payload = { "Age" : 18, "Risk category" : "High", "Debt review" : "false" };
+        var payload = { "Age": 18, "Risk category": "High", "Debt review": "false" };
         models.DecisionTable.exec("RoutingRulesOutput", payload, bootstrap.defaultContext, function (err, result) {
             if (err) {
                 done(err);
@@ -59,22 +60,22 @@ describe(chalk.blue('Decision table evaluation'), function () {
                 expect(result.Routing.length).to.be.equal(2);
                 expect(result.Routing[1]).to.contain('Accept');
                 expect(result.Routing[0]).to.contain('Refer');
-                
+
                 expect(result['Review level'].length).to.be.equal(2);
                 expect(result['Review level'][1]).to.contain('None');
                 expect(result['Review level'][0]).to.contain('Level1');
-                
+
                 expect(result.Reason.length).to.be.equal(2);
                 expect(result.Reason[1]).to.contain('Acceptable');
                 expect(result.Reason[0]).to.contain('High risk application');
-                
+
                 done();
             }
         });
     });
 
     it('Collect hit policy without any operator', function (done) {
-        var payload = { "Age" : 18, "Risk category" : "High", "Debt review" : "false" };
+        var payload = { "Age": 18, "Risk category": "High", "Debt review": "false" };
         models.DecisionTable.exec("RoutingRules", payload, bootstrap.defaultContext, function (err, result) {
             if (err) {
                 done(err);
@@ -97,7 +98,7 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Collect hit policy with + operator for numbers', function (done) {
-        var payload = { "State" : "Karnataka", "Units" : 150 };
+        var payload = { "State": "Karnataka", "Units": 150 };
         models.DecisionTable.exec("ElectricityBill", payload, bootstrap.defaultContext, function (err, result) {
             expect(result.Amount).to.equal(693);
             done();
@@ -105,7 +106,7 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Collect hit policy with < operator for numbers', function (done) {
-        var payload = { "Age" : 100, "Years of Service" : 200 };
+        var payload = { "Age": 100, "Years of Service": 200 };
         models.DecisionTable.exec("HolidaysMin", payload, bootstrap.defaultContext, function (err, result) {
             expect(result.Holidays).to.equal(3);
             done();
@@ -113,23 +114,23 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Collect hit policy with > operator for numbers', function (done) {
-        var payload = { "Age" : 100, "Years of Service" : 200 };
-        models.DecisionTable.exec("HolidaysMax", payload, bootstrap.defaultContext, function(err, result) {
+        var payload = { "Age": 100, "Years of Service": 200 };
+        models.DecisionTable.exec("HolidaysMax", payload, bootstrap.defaultContext, function (err, result) {
             expect(result.Holidays).to.equal(22);
             done();
         });
     });
 
     it('Collect hit policy with # operator for numbers', function (done) {
-        var payload = { "Age" : 100, "Years of Service" : 200 };
-        models.DecisionTable.exec("HolidaysCount", payload, bootstrap.defaultContext, function(err, result) {
+        var payload = { "Age": 100, "Years of Service": 200 };
+        models.DecisionTable.exec("HolidaysCount", payload, bootstrap.defaultContext, function (err, result) {
             expect(result.Holidays).to.equal(3);
             done();
         });
     });
 
     it('Collect hit policy with + operator for strings', function (done) {
-        var payload = { "loanAmount" : 2000, "salary" : 20000 };
+        var payload = { "loanAmount": 2000, "salary": 20000 };
         models.DecisionTable.exec("MembershipSum", payload, bootstrap.defaultContext, function (err, result) {
             expect(result.membership).to.equal("SILVER GENERAL");
             done();
@@ -137,7 +138,7 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Collect hit policy with < operator for strings', function (done) {
-        var payload = { "loanAmount" : 30000, "salary" : 60000 };
+        var payload = { "loanAmount": 30000, "salary": 60000 };
         models.DecisionTable.exec("MembershipMin", payload, bootstrap.defaultContext, function (err, result) {
             expect(result.membership).to.equal("GENERAL");
             done();
@@ -145,7 +146,7 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Collect hit policy with > operator for strings', function (done) {
-        var payload = { "loanAmount" : 12000, "salary" : 110000 };
+        var payload = { "loanAmount": 12000, "salary": 110000 };
         models.DecisionTable.exec("MembershipMax", payload, bootstrap.defaultContext, function (err, result) {
             expect(result.membership).to.equal("SILVER");
             done();
@@ -153,7 +154,7 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Collect hit policy with # operator for strings', function (done) {
-        var payload = { "loanAmount" : 1000, "salary" : 200000 };
+        var payload = { "loanAmount": 1000, "salary": 200000 };
         models.DecisionTable.exec("MembershipCount", payload, bootstrap.defaultContext, function (err, result) {
             expect(result.membership).to.equal(4);
             done();
@@ -161,9 +162,9 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Collect hit policy with + operator for boolean', function (done) {
-        var payload1 = { "age" : 40, "salary" : 55000 };//T, T
-        var payload2 = { "age" : 18, "salary" : 6000 };//F, F
-        var payload3 = { "age" : 30, "salary" : 55000 };//T, T, F
+        var payload1 = { "age": 40, "salary": 55000 };//T, T
+        var payload2 = { "age": 18, "salary": 6000 };//F, F
+        var payload3 = { "age": 30, "salary": 55000 };//T, T, F
         models.DecisionTable.exec("LoanEligibilitySum", payload3, bootstrap.defaultContext, function (err3, result3) {
             expect(result3.LoanEligibility).to.equal(false);
             models.DecisionTable.exec("LoanEligibilitySum", payload2, bootstrap.defaultContext, function (err2, result2) {
@@ -177,9 +178,9 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Collect hit policy with < operator for boolean', function (done) {
-        var payload1 = { "age" : 40, "salary" : 55000 };//T, T
-        var payload2 = { "age" : 18, "salary" : 6000 };//F, F
-        var payload3 = { "age" : 30, "salary" : 55000 };//T, T, F
+        var payload1 = { "age": 40, "salary": 55000 };//T, T
+        var payload2 = { "age": 18, "salary": 6000 };//F, F
+        var payload3 = { "age": 30, "salary": 55000 };//T, T, F
         models.DecisionTable.exec("LoanEligibilityMin", payload3, bootstrap.defaultContext, function (err3, result3) {
             expect(result3.LoanEligibility).to.equal(0);
             models.DecisionTable.exec("LoanEligibilityMin", payload2, bootstrap.defaultContext, function (err2, result2) {
@@ -193,9 +194,9 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Collect hit policy with > operator for boolean', function (done) {
-        var payload1 = { "age" : 40, "salary" : 55000 };//T, T
-        var payload2 = { "age" : 18, "salary" : 6000 };//F, F
-        var payload3 = { "age" : 30, "salary" : 55000 };//T, T, F
+        var payload1 = { "age": 40, "salary": 55000 };//T, T
+        var payload2 = { "age": 18, "salary": 6000 };//F, F
+        var payload3 = { "age": 30, "salary": 55000 };//T, T, F
         models.DecisionTable.exec("LoanEligibilityMax", payload3, bootstrap.defaultContext, function (err3, result3) {
             expect(result3.LoanEligibility).to.equal(1);
             models.DecisionTable.exec("LoanEligibilityMax", payload2, bootstrap.defaultContext, function (err2, result2) {
@@ -209,9 +210,9 @@ describe(chalk.blue('Decision table evaluation'), function () {
     });
 
     it('Collect hit policy with # operator for boolean', function (done) {
-        var payload1 = { "age" : 40, "salary" : 55000 };//T, T
-        var payload2 = { "age" : 18, "salary" : 6000 };//F, F
-        var payload3 = { "age" : 30, "salary" : 55000 };//T, T, F
+        var payload1 = { "age": 40, "salary": 55000 };//T, T
+        var payload2 = { "age": 18, "salary": 6000 };//F, F
+        var payload3 = { "age": 30, "salary": 55000 };//T, T, F
         models.DecisionTable.exec("LoanEligibilityCount", payload3, bootstrap.defaultContext, function (err3, result3) {
             expect(result3.LoanEligibility).to.equal(2);
             models.DecisionTable.exec("LoanEligibilityCount", payload2, bootstrap.defaultContext, function (err2, result2) {
