fb7bc17559fcafbff7689bfebd409ade3e499fd8
Ramesh <rameshchoudhary_s@@edgeverve.com>
2017-08-22 14:30:35 +0530

    change name of variantOf model

--- common/models/framework/model-definition.js
+++ common/models/framework/model-definition.js
@@ -167,6 +167,7 @@ module.exports = function ModelDefintionFn(modelDefinition) {
       }
       if (modeldefinition.variantOf) {
         modeldefinition.plural = baseModel.pluralModelName;
+        modeldefinition.name = baseModel.name;
       }
       return mongoSpecificHandling(modeldefinition, ctx, next);
     });
