026666d3e3cd131f0f28eb89102c8f60b739830a
ajith <ajith_v@edgeverve.com>
2017-12-11 11:12:49 +0530

    Fixed lint errir

--- common/models/framework/decision-graph.js
+++ common/models/framework/decision-graph.js
@@ -16,42 +16,41 @@ var { createDecisionGraphAST, executeDecisionService } = jsFeel.decisionService;
 
 
 module.exports = function (DecisionGraph) {
-
 // Remote method to execute a Decision Service with data POSTed from the Rule Designer
-DecisionGraph.remoteMethod('execute', {
-		description: 'Executes a Decision Service Payload Posted from the Rule Designer',
-		accessType: 'WRITE',
-		isStatic: true,
-    accepts: [{ arg: 'inputData', type: 'object', http: { source: 'body' }, 
-      required: true, description: "The JSON containing the graph data and payload to execute" }
-		],
-		http: {
-			verb: 'POST',
-			path: '/execute'
-		},
-		returns: {
-			type: 'object',
-			root: true
-		}
-	});
+  DecisionGraph.remoteMethod('execute', {
+    description: 'Executes a Decision Service Payload Posted from the Rule Designer',
+    accessType: 'WRITE',
+    isStatic: true,
+    accepts: [{ arg: 'inputData', type: 'object', http: { source: 'body' },
+      required: true, description: 'The JSON containing the graph data and payload to execute' }
+    ],
+    http: {
+      verb: 'POST',
+      path: '/execute'
+    },
+    returns: {
+      type: 'object',
+      root: true
+    }
+  });
 
   // Executes a Decision Service with data POSTed from the Rule Designer
   DecisionGraph.execute = function (inputData, options, cb) {
     var decisionMap = inputData.jsonFeel;
     var decisions = inputData.decisions;
     var payload = inputData.payload;
-  
+
     var ast = createDecisionGraphAST(decisionMap);
-  
+
     var promises = decisions.map(d => executeDecisionService(ast, d, payload));
-  
+
     Promise.all(promises).then(answers => {
       cb(null, answers);
     }).catch(err => {
-      console.error(err);
+      log.error(err);
       cb(serialize(err), null);
     });
-  }
+  };
 
   DecisionGraph.observe('before save', function DecisionGraphBeforeSaveFn(
     ctx,
@@ -59,7 +58,7 @@ DecisionGraph.remoteMethod('execute', {
   ) {
     var dataObj = ctx.instance || ctx.data;
     var document = ctx.options.graphDocument;
-    if(!document) return next();
+    if (!document) return next();
     var base64String = document.documentData.split(',')[1];
     // var binaryData = Buffer.from(base64String, 'base64').toString('binary');
     var binaryData = new Buffer(base64String, 'base64').toString('binary');
