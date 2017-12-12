987ea73a83595ed16d89fc92105497334a072ac4
atul <atul@PHOENIXX.ad.infosys.com>
2017-12-12 14:30:22 +0530

    fixing eslint

--- server/boot/uws-boot.js
+++ server/boot/uws-boot.js
@@ -1,6 +1,4 @@
 var {WSClient} = require('../../lib/uws-client.js');
-
-
 module.exports = function (app, cb) {
   var sockethost = process.env.TX_ROUTER_HOST || 'localhost';
   var socketurl = 'ws://' + sockethost + ':3183';
@@ -13,41 +11,11 @@ module.exports = function (app, cb) {
     c.connected = true;
     app.set('oe-tx-router', c);
   });
-
-
-  // var sockethost = process.env.TX_ROUTER_HOST || 'localhost';
-  // var socketurl = 'http://' + sockethost + ':3183';
-
-//  var socket = require("socket.io-client")("http://localhost:3183");
-  // var socket = require("socket.io-client")(socketurl);
   var remotes = app.remotes();
-
   var ProxyContext = require('../../lib/proxy-context');
-
-  // socket.on("connect", function() {
-  //   console.log("connected ");
-  //   app.set('oe-tx-router',socket);
-  //   var app2 = app;
-  // });
-
-  // socket.on("disconnect", function() {
-  //   console.log('disconnected');
-  //   setTimeout(function(){
-  //       socket.connect();
-  //   }, 200);
-  // });
-
-
-  // socket.on("error", function() {
-  //     console.log('web socket io error ');
-  // });
-
   var methods = {};
-
   var socket = c;
-
   var counter = 0;
-
   socket.on('process', function (adata) {
     // var data = adata.data;
     var data = adata.msg;
@@ -55,14 +23,10 @@ module.exports = function (app, cb) {
     if ( counter > 500 ) {
       counter = 0;
     }
-
-
     var callback = function (err, result) {
       adata.msg = {error: err, result: result};
       socket.emit('processed', adata);
-      // fn(processResponse);
     };
-
     var method = methods[data.fullMethodName];
     if (!method) {
       method = remotes.findMethod(data.fullMethodName);
@@ -84,10 +48,8 @@ module.exports = function (app, cb) {
       }
       request.callContext.evproxyModelPlural = data.modelPlural;
       request.callContext.evproxyModelId = data.id;
-
       var ctx = new ProxyContext(request, data.ctorArgs, data.args);
       ctx.method = method;
-
       if (method.isStatic) {
         ctx.invoke(method.ctor, method, function (err, result) {
           if (err) return callback(err);
@@ -95,10 +57,8 @@ module.exports = function (app, cb) {
           callback(err, ctx.result);
         });
       } else {
-         // invoke the shared constructor to get an instance
         ctx.invoke(method.ctor, method.sharedCtor, function (err, inst) {
           if (err) return callback(err);
-               // invoke the instance method
           ctx.invoke(inst, method, function (err, result) {
             if (err) return callback(err);
             ctx.result = result;
