03b8ced9964561a8fff689133dd49e82234f02d8
Maya Bar <Maya.Bar@ad.infosys.com>
2018-01-18 14:10:58 +0200

    Fix async-service-test

--- test/async-service-tests/prepare-test.js
+++ test/async-service-tests/prepare-test.js
@@ -9,8 +9,8 @@ var fs = require('fs');
 fs.readFile('server/model-config.json', 'utf8', function(oErr, sText) {
     var r1 = sText.substr(sText.length - 4, sText.length - 1);
     var re = new RegExp(r1,"g");
-     var result = sText.replace(r1, '},\n\t"Inventory": {"public": true,"dataSource": "db"},\n\t"InventoryTransaction": {"public": true,"dataSource": "db"}\n}');
-     fs.writeFile('test/actor-tests/test-files/server/model-config.json', result, function (err) {
+     var result = sText.replace(r1, '},\n\t"RetryTestModel": {"public": true,"dataSource": "db"}\n}');
+     fs.writeFile('test/async-service-tests/test-files/server/model-config.json', result, function (err) {
         if(err) {
             return process.exit(-1);
         }
