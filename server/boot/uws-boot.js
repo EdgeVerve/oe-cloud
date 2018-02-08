f20fa0f752c993d2ca4bfdea65880ff415c2ded4
rameshchoudhary <ramesh_srch@outlook.com>
2018-02-08 12:56:33 +0530

    updated package.json references

--- server/boot/uws-boot.js
+++ server/boot/uws-boot.js
@@ -56,6 +56,7 @@ module.exports = function (app, cb) {
       }
       if (data.lock) {
         request.callContext.lockMode = DB_LOCK_MODE;
+        global.setDBLockMode();
       }
       request.callContext.evproxyModelPlural = data.modelPlural;
       request.callContext.evproxyModelId = data.id;
