77d606622a8daba88511331b57c5d694d5a39d35
deostroll <deostroll@gmail.com>
2018-03-16 11:34:35 +0530

    updated the cluster test

--- test/rule-engine-cluster-tests/cluster-tests.js
+++ test/rule-engine-cluster-tests/cluster-tests.js
@@ -1,7 +1,19 @@
 const chalk = require('chalk');
+const https = require('https');
+const assert = require('assert');
 
-describe(chalk.blue('Cluster tests'), function(){
-  it('should simply run awesomly', done => {
-    setTimeout(done, 5000);
+describe(chalk.blue('rule cluster tests'), function(){
+  it('should assert node1 is up', done => {
+    https.get('https://test.node1.oecloud.local/status', res => {
+      assert(res.statusCode === 200, "Expected 200 OK status. Instead got: " + res.statusCode);
+      done();
+    });
+  });
+
+  it('should assert node2 is up', done => {
+    https.get('https://test.node2.oecloud.local/status', res => {
+      assert(res.statusCode === 200, "Expected 200 OK status. Instead got: " + res.statusCode);
+      done();
+    });
   });
 });
